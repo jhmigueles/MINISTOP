703458c64227158e35cdca4f3315200234364833
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-04-29 19:50:00 +0200

    title

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,7 +10,7 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic 
     fluidPage(
-      h1("MINISTOP"),
+      h1("MINISTOP longitudinal associations"),
       mod_ilrs4_ui("ilrs4_ui_1")
     )
   )
